b34e6b6bf86ee4f9e9cb5443d3b144929c8aea6c
Raúl Acosta <raulacostadeveloper@outlook.com>
2024-07-21 18:40:04 -0600

    functionality

--- src/app/pagina/page.tsx
+++ src/app/pagina/page.tsx
@@ -2,7 +2,7 @@ import { ContainerColorWeel } from "./Color-Wheel/ContainerColorWeel";
 
 export default function Home() {
     return (
-        <div>
+        <div className="nose">
             {/* Yo are able to use this element */}
             <ContainerColorWeel/>
         </div>
